c3169e6a84592a4d27f93b7ae76acf1b00bcb5b7
dasJintu <77599234+dasJintu@users.noreply.github.com>
2021-09-18 18:20:25 +0530

    first-commit

---
+++ components/Navbar.jsx
@@ -0,0 +1,11 @@
+function Navbar() {
+  return (
+    <div>
+      <h2 className="text-lg font-bold text-indigo-700">
+        Barney's Breakfast Club
+      </h2>
+    </div>
+  );
+}
+
+export default Navbar;

ea622d59944a95b3df64cdc067fa441c44ee24a7
dasJintu <77599234+dasJintu@users.noreply.github.com>
2021-09-23 20:01:04 +0530

    update

--- components/Description.jsx
+++ components/Description.jsx
@@ -20,7 +20,7 @@ function Description() {
             GREAT TASTE, GOOD TIMES
           </h1>
           <p className="text-gray-600">
-            Here at Barney’s Burgers we take pride in the quality of the meat
+            Here at Grassfeed Grills we take pride in the quality of the meat
             used for each and every burger. If it’s not perfect and grilled just
             the way you ordered it, we won’t send it out. We guarantee the
             burger delivered to your table is the best burger you could have
